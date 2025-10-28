ac84c9804afea3960dd7d9a0b61eb73afec8eaae
Brendan Roof <brendan@priorlabs.ai>
2025-10-28 19:48:17 +0100

    update docstring

--- tabpfn_client/estimator.py
+++ tabpfn_client/estimator.py
@@ -103,7 +103,7 @@ class TabPFNClassifier(ClassifierMixin, BaseEstimator, TabPFNModelSelection):
              predictive performance when there are many classes or when calibrating the
              model's confidence. This is only applied when predicting during a
              post-processing.
-        ignore_pretraining_limits: bool, default=False
+        ignore_pretraining_limits: bool, default=True
             Whether to ignore the pre-training limits of the model. The TabPFN models
             have been pre-trained on a specific range of input data. If the input data
             is outside of this range, the model may not perform well. You may ignore
