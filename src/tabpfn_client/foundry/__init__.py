3cfa5fda2cd3487a5b07ce6c801bf47e36b86476
Dominik Safaric <153173064+safaricd@users.noreply.github.com>
2026-06-17 11:47:22 +0200

    Support for running TabPFN on AI Foundry via the client SDK (#310)

---
+++ src/tabpfn_client/foundry/__init__.py
@@ -0,0 +1,11 @@
+#  Copyright (c) Prior Labs GmbH 2026.
+#  Licensed under the Apache License, Version 2.0
+"""Azure AI Foundry client for TabPFN.
+
+from tabpfn_client.foundry import TabPFNClassifier, TabPFNRegressor
+"""
+
+from tabpfn_client.foundry.estimator import TabPFNClassifier, TabPFNRegressor
+
+
+__all__ = ["TabPFNClassifier", "TabPFNRegressor"]
