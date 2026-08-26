a9c38ac7e911d42099bcbdf018e62a2e703fd39f
ggprior <georg@priorlabs.ai>
2026-08-26 11:39:29 +0000

    ci: add release pipeline and towncrier changelog (#361)

--- src/tabpfn_client/__init__.py
+++ src/tabpfn_client/__init__.py
@@ -1,6 +1,8 @@
 #  Copyright (c) Prior Labs GmbH 2025.
 #  Licensed under the Apache License, Version 2.0
 
+from importlib.metadata import PackageNotFoundError, version
+
 from tabpfn_client.config import (
     init,
     reset,
@@ -12,7 +14,13 @@ from tabpfn_client.estimator import TabPFNClassifier, TabPFNRegressor
 from tabpfn_client.interactive_auth import InteractiveLoginError, interactive_login
 from tabpfn_client.service_wrapper import UserDataClient
 
+try:
+    __version__ = version("tabpfn-client")
+except PackageNotFoundError:
+    __version__ = "0.0.0.dev0"
+
 __all__ = [
+    "__version__",
     "init",
     "reset",
     "TabPFNClassifier",
